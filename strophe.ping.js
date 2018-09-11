1c9bd09252c7e5fb97fb001b71c87fb6ac77a268
Jason Thomas <jthomas@hirevue.com>
2018-09-11 11:29:58 -0600

    Make work with new Strophe package structure.

--- strophe.ping.js
+++ strophe.ping.js
@@ -1,54 +1,56 @@
-let Strophe = require("strophe.js").Strophe;
+let StrophePackage = require('strophe.js');
+let Strophe = StrophePackage.Strophe;
 
-Strophe.addConnectionPlugin("ping", {
+Strophe.addConnectionPlugin('ping', {
   _c: null,
 
   // called by the Strophe.Connection constructor
   init: function(conn) {
     this._c = conn;
-    Strophe.addNamespace("PING", "urn:xmpp:ping");
+    Strophe.addNamespace('PING', 'urn:xmpp:ping');
   },
 
   /**
-     * Function: ping
-     *
-     * Parameters:
-     * (String) to - The JID you want to ping
-     * (Function) success - Callback function on success
-     * (Function) error - Callback function on error
-     * (Integer) timeout - Timeout in milliseconds
-     */
+   * Function: ping
+   *
+   * Parameters:
+   * (String) to - The JID you want to ping
+   * (Function) success - Callback function on success
+   * (Function) error - Callback function on error
+   * (Integer) timeout - Timeout in milliseconds
+   */
   ping: function(jid, success, error, timeout) {
-    var id = this._c.getUniqueId("ping");
-    var iq = Strophe.$iq({ type: "get", to: jid, id: id }).c("ping", {
-      xmlns: Strophe.NS.PING
+    var id = this._c.getUniqueId('ping');
+    var iq = StrophePackage.$iq({ type: 'get', to: jid, id: id }).c('ping', {
+      xmlns: Strophe.NS.PING,
     });
     this._c.sendIQ(iq, success, error, timeout);
   },
 
   /**
-     * Function: pong
-     *
-     * Parameters:
-     * (Object) ping - The ping stanza from the server.
-     */
+   * Function: pong
+   *
+   * Parameters:
+   * (Object) ping - The ping stanza from the server.
+   */
   pong: function(ping) {
-    var from = ping.getAttribute("from");
-    var id = ping.getAttribute("id");
-    var iq = Strophe.$iq({ type: "result", to: from, id: id });
+    var from = ping.getAttribute('from');
+    var id = ping.getAttribute('id');
+    var iq = StrophePackage.$iq({ type: 'result', to: from, id: id });
     this._c.sendIQ(iq);
   },
 
   /**
-     * Function: addPingHandler
-     *
-     * Parameters:
-     * (Function) handler - Ping handler
-     *
-     * Returns:
-     * A reference to the handler that can be used to remove it.
-     */
+   * Function: addPingHandler
+   *
+   * Parameters:
+   * (Function) handler - Ping handler
+   *
+   * Returns:
+   * A reference to the handler that can be used to remove it.
+   */
   addPingHandler: function(handler) {
-    return this._c.addHandler(handler, Strophe.NS.PING, "iq", "get");
-  }
+    return this._c.addHandler(handler, Strophe.NS.PING, 'iq', 'get');
+  },
 });
+
